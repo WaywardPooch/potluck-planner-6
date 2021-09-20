bab670ea456c6126a11e2ef1f46a5e6bc880122c
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-09-20 08:28:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Potluck Planner 6</h1>
+      <p>Welcome to the project!</p>
+    </div>
+  );
+}
+
+export default App;
