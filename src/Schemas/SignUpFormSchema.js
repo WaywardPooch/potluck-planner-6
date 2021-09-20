910460333fa14e1b8e604ec23888d080118d7025
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-20 15:07:02 -0700

    added error message for a password under 8 characters

--- src/Schemas/SignUpFormSchema.js
+++ src/Schemas/SignUpFormSchema.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 let schema = yup.object().shape({
     name: yup.string().required('A name is required'),
     email: yup.string().required('An email is required').email('A valid email is required'),
-    password: yup.string().required('A password is required').min(8),
+    password: yup.string().required('A password is required').min(8, 'Your password must be at least 8 characters'),
     confirmPw: yup.string().required('You must confirm your password').oneOf([yup.ref('password')], 'The passwords must be the same'),
 });
 
