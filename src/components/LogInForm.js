3919bf31b0846eb3b4f14549bfd793982217e6a6
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-20 11:05:10 -0700

    reactstrap added properly and made a basic form

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,6 +1,12 @@
+import { Form, FormGroup, Input } from 'reactstrap';
+
 function LogInForm(props){
     return (
-        <></>
+        <Form>
+            <FormGroup>
+                <Input type='submit'/>
+            </FormGroup>
+        </Form>
     );
 }
 
