255562c9d19a1ec4e44234b62151e3fb5bc40edb
Your Name <jbradshaw0526@gmail.com>
2021-09-21 16:34:00 -0500

    fix

--- src/scenes/CreatePotluckSection/CreatePotluckSection.jsx
+++ src/scenes/CreatePotluckSection/CreatePotluckSection.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 // Styles
 import "./CreatePotluckSection.scss";
+import CreatPotluck from "../../components/CreatePotluckForm.js"
+
 
 const CreatePotluckSection = () => {
   return (
     <section>
       <p>Create a potluck</p>
+      <CreatPotluck />
     </section>
   );
 };
