f6c78e67eef5a057debafdb38f3d674c1c0e61e0
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-23 10:35:59 -0700

    Display Form Submission Error for Log In Form and Sign Up Form

--- src/components/forms/LogInForm.js
+++ src/components/forms/LogInForm.js
@@ -23,6 +23,8 @@ function LogInForm(props) {
   const [formData, setFormData] = useState(initialFormData);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
   const [loggedIn, setLoggedIn] = useState(false); //TEMP CODE, PLEASE DELETE
+  const [displayError, setDisplayError] = useState(false);
+  const [currentError, setCurrentError] = useState(initialFormErrors.email);
 
   // ========== EVENT HANDLERS
   const onSubmit = (event) => {
@@ -36,18 +38,20 @@ function LogInForm(props) {
             formData.password === usersDummyData[i].password
           ) {
             //TEMP CODE, PLEASE DELETE
+            setDisplayError(false);
             setLoggedIn(true); //TEMP CODE, PLEASE DELETE
             break; //TEMP CODE, PLEASE DELETE
           } //TEMP CODE, PLEASE DELETE
           if (i === usersDummyData.length - 1) {
             //TEMP CODE, PLEASE DELETE
-            console.error("Incorrect email or password submitted"); //TEMP CODE, PLEASE DELETE
+            setCurrentError("Incorrect email or password submitted"); //TEMP CODE, PLEASE DELETE
+            setDisplayError(true);
           } //TEMP CODE, PLEASE DELETE
         } //TEMP CODE, PLEASE DELETE
         setFormData(initialFormData);
         setFormErrors(initialFormErrors);
       } else {
-        console.error(formErrors);
+        setDisplayError(true);
       }
     });
   };
@@ -57,14 +61,15 @@ function LogInForm(props) {
     yup
       .reach(schema, name)
       .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
-      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+      .then(() => {setFormErrors({ ...formErrors, [name]: "" }); setCurrentError(name==='email' ? formErrors.password : formErrors.email)})
+      .catch((err) => {setFormErrors({ ...formErrors, [name]: err.errors[0] }); setCurrentError(err.errors[0]);});
   };
 
   // ========== MARKUP
   return (
     <form onSubmit={onSubmit}>
       {loggedIn && <p>You are logged in</p>} {/*TEMP CODE, PLEASE DELETE*/}
+      {displayError && <p style={{color: 'red'}}>{currentError}</p>}
       <label>
         Email:
         <input
--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -26,6 +26,9 @@ function SignUpForm() {
   // ========== STATES
   const [formData, setFormData] = useState(initialFormData);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [createdAccount, setCreatedAccount] = useState(false); //TEMP CODE, PLEASE DELETE
+  const [displayError, setDisplayError] = useState(false);
+  const [currentError, setCurrentError] = useState(initialFormErrors.email);
 
   // ========== EVENT HANDLERS
   const onSubmit = (event) => {
@@ -33,25 +36,48 @@ function SignUpForm() {
     schema.isValid(formData).then((valid) => {
       if (valid) {
         usersDummyData.push(formData); //TEMP CODE, PLEASE DELETE
+        setCreatedAccount(true); //TEMP CODE, PLEASE DELETE
         setFormData(initialFormData);
+        setDisplayError(false);
       } else {
-        console.error(formErrors);
+        setDisplayError(true);
       }
     });
   };
   const onChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
-    yup
+    if(name==='confirmPw'){
+      if(formData.password===value){
+        setFormErrors({ ...formErrors, [name]: ''});
+        setCurrentError(formErrors.email ? formErrors.email : formErrors.name ? formErrors.name : formErrors.password);
+      } else {
+        setFormErrors({ ...formErrors, [name]:value ? 'The passwords must be the same' : initialFormErrors[name]});
+        setCurrentError(value ? 'The passwords must be the same' : initialFormErrors[name]);
+      }
+    } else {
+      if(name==='password'){
+        if(formData.confirmPw===value){
+          setFormErrors({ ...formErrors, confirmPw: ''});
+          setCurrentError(formErrors.email ? formErrors.email : formErrors.name ? formErrors.name : formErrors.password);
+        } else {
+          setFormErrors({ ...formErrors, confirmPw:value ? 'The passwords must be the same' : initialFormErrors['confirmPw']});
+          setCurrentError(value ? 'The passwords must be the same' : initialFormErrors['confirmPw']);
+        }
+      }
+      yup
       .reach(schema, name)
       .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
-      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+      .then(() => {setFormErrors({ ...formErrors, [name]: "" }); setCurrentError(name!=='email'&&formErrors.email ? formErrors.email : name!=='name'&&formErrors.name ? formErrors.name : name!=='password'&&formErrors.password ? formErrors.password : formErrors.confirmPw);})
+      .catch((err) => {setFormErrors({ ...formErrors, [name]: err.errors[0] }); setCurrentError(err.errors[0]);});
+    }
   };
 
   // ========== MARKUP
   return (
     <form onSubmit={onSubmit}>
+      {createdAccount && <p>You have created an account</p>} {/*TEMP CODE, PLEASE DELETE*/}
+      {displayError && <p style={{color: 'red'}}>{currentError}</p>}
       <label>
         Name:
         <input
